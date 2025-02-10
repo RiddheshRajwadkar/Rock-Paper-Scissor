22f90b4de7f5fb6efb409e469c0403743c545516
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2025-02-10 21:17:12 +0530

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -7,10 +7,37 @@ function computerChoice(){
     const computerChoice = choice[Math.floor(Math.random() * choice.length)];
     return computerChoice;
 }
-computerChoice();
 
 function humanChoice(){
     const humanChoice = prompt("Enter your choice (rock, paper, scissors): ");
     return humanChoice;
 }
-humanChoice();
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log("It's a tie!");
+    } else if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    } else if(humanChoice === "rock" && computerChoice === "paper"){
+        console.log("You lose! Paper beats rock.");
+        computerScore++;
+    } else if(humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    } else if(humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+    } else if(humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    } else if(humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+    }
+}
+
+const humanSelection = humanChoice().toLowerCase();
+const computerSelection = computerChoice().toLowerCase();
+
+playRound(humanSelection, computerSelection);
